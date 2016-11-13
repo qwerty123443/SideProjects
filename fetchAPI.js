c43e41cee1d10559f00163757288038fb583cc5b
Vic <Jantje19@users.noreply.github.com>
2016-11-13 15:14:54 +0100

    Add files via upload

---
+++ fetchAPI.js
@@ -0,0 +1,36 @@
+function fetchAPI(url, responseType) {
+	return new Promise( (resolve, reject) => {
+		var req = new XMLHttpRequest();
+
+		req.open('GET', url);
+
+		req.onload = function() {
+			if (req.status == 200) {
+				resolve(responseProcess(req.response, responseType));
+			}
+			else {
+				reject(Error(req.statusText));
+			}
+		};
+
+		req.onerror = function() {
+			reject(Error("Network Error"));
+		};
+
+		req.send();
+	});
+}
+
+function responseProcess(response, responseType) {
+	switch (responseType) {
+		case 'text':
+			return response;
+			break;
+		case 'json':
+			return JSON.parse(response)
+			break;
+		default:
+			return response;
+			break;
+	}
+}
