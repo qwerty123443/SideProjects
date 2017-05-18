3cd27667038a2a973d3b97bee403de489c679ffd
Vic <Jantje19@users.noreply.github.com>
2017-05-18 11:53:42 +0200

    Add files via upload

--- toast.js
+++ toast.js
@@ -6,8 +6,11 @@
 
 
 class Toast {
-	constructor(stayTime, animation, styles) {
+	constructor(stayTime, animation, styles, debug) {
+		this.debug = debug;
 		this._error = false;
+
+		if (debug == null || debug == undefined) this.debug = true;
 
 		if (stayTime < 0) {
 			console.error('Toast stay time is lower than 1');
@@ -17,13 +20,18 @@ class Toast {
 		} else {
 			if (!stayTime) {
 				stayTime = 2000;
-				console.warn('Toast stay time is not set, resetting to default');
+				if (this.debug) console.warn('Toast stay time is not set, resetting to default');
 			}
 
-			this._error = false;
 			this.styles = styles;
 			this.stayTime = stayTime;
 			this.animation = animation;
+
+			if (!animation) {
+				if (this.debug)
+					console.log('Toast animation is not set, resetting to default');
+				this.animation = {fadeTime: 100, speedCurve: 'ease', type: 'opacity'};
+			}
 
 			this._toastContainer = document.createElement('div');
 			this._toastContainer.style.right = '20px';
@@ -83,11 +91,11 @@ class Toast {
 				// Animation request
 				if (this.animation) {
 					if (this.animation.fadeTime == null || this.animation.fadeTime <= 0) {
-						console.warn('Animation time is not set, resetting to default');
+						if (this.debug) console.warn('Animation time is not set, resetting to default');
 					} else if (!this.animation.speedCurve) {
-						console.warn('Animation speed curve is not set, resetting to default');
+						if (this.debug) console.warn('Animation speed curve is not set, resetting to default');
 					} else if (!this.animation.type) {
-						console.warn('Animation type is not set, resetting to default');
+						if (this.debug) console.warn('Animation type is not set, resetting to default');
 					}
 
 					const type = this.animation.type;
@@ -105,7 +113,7 @@ class Toast {
 						break;
 						default:
 						toast.style.opacity = '0';
-						console.log('Animation type is not correct, resetting to default');
+						if (this.debug) console.log('Animation type is not correct, resetting to default');
 						break;
 					}
 				} else {
@@ -131,7 +139,8 @@ class Toast {
 				toast.appendChild(textElem);
 
 				// Buttons
-				if (buttons) {
+				if (buttons && buttons.length > 0) {
+					this.stayTime = 'infinite';
 					let buttonDiv = document.createElement('div');
 
 					buttonDiv.style.marginLeft = '10px';
@@ -154,7 +163,8 @@ class Toast {
 								}, false);
 							} else {
 								button.addEventListener('click', evt => {
-									object.onclick(evt);
+									this.remove(toast);
+									if (object.onclick) object.onclick(evt);
 								}, false);
 							}
 
@@ -162,6 +172,7 @@ class Toast {
 							if (object.events) {
 								for (key in object.events) {
 									button.addEventListener(key, evt => {
+										this.remove(toast);
 										object.events[key](evt);
 									}, false);
 								}
@@ -171,7 +182,7 @@ class Toast {
 
 							buttonDiv.appendChild(button);
 						} else {
-							console.error('Button name not set');
+							if (this.debug) console.error('Button name not set');
 						}
 					});
 
@@ -190,7 +201,7 @@ class Toast {
 
 					if (!this.styles.color) {
 						toast.style.backgroundColor = '#3B3B4F';
-						console.warn('Toast background color is not set, resetting to default');
+						if (this.debug) console.warn('Toast background color is not set, resetting to default');
 					}
 				}
 
@@ -199,34 +210,38 @@ class Toast {
 				// Animation
 				window.requestAnimationFrame(() => {
 					window.requestAnimationFrame(() => {
-						const type = this.animation.type;
+						if (this.animation.type) {
+							switch (this.animation.type) {
+								case 'bottom':
+								toast.style.bottom = '0px';
+								break;
+								case 'opacity':
+								toast.style.opacity = '1';
+								break;
+								default:
+								toast.style.opacity = '1';
+								if (this.debug) console.log('Animation type is not correct, resetting to default');
+								break;
+							}
+						} else {if (this.debug) console.warn('Animation type is not correct'); toast.style.opacity = '1';}
 
-						switch (type) {
-							case 'bottom':
-							toast.style.bottom = '0px';
-							break;
-							case 'opacity':
-							toast.style.opacity = '1';
-							break;
-							default:
-							toast.style.opacity = '1';
-							console.log('Animation type is not correct, resetting to default');
-							break;
+						if (this.stayTime == 'infinite') {
+							if (this.debug) console.log('Toast will stay forever, unless the remove function is called');
+						} else {
+							setTimeout(() => {
+								this.remove(toast);
+							}, this.stayTime);
 						}
-
-						setTimeout(() => {
-							this.remove(toast);
-						}, this.stayTime);
 					});
 				});
-
-				return toast;
 			} else {
-				console.error('Toast text is not set');
+				if (this.debug) console.error('Toast text is not set');
 			}
 		} else {
-			console.error('There was a previous error');
+			if (this.debug) console.error('There was a previous error');
 		}
+
+		return this;
 	}
 
 	remove(toast) {
@@ -235,188 +250,7 @@ class Toast {
 		setTimeout( () => {
 			try {
 				this._toastContainer.removeChild(toast);
-			} catch (err) {console.log('Toast was already removed, or an error occured')}
+			} catch (err) {if (this.debug) console.log('Toast was already removed, or an error occured')}
 		}, 1100);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-/*class Toast {
-	constructor(stayTime, animation, buttons, styles) {
-		this._buttons = buttons;
-		this.stayTime = stayTime;
-		this._animaion = animation;
-
-		// Add element
-		this._toast = document.createElement('div');
-
-		// Style element
-		this._toast.style.color = 'white';
-		this._toast.style.padding = '10px 20px';
-		this._toast.style.backgroundColor = '#3B3B4F';
-		this._toast.style.transition = 'opacity 1s ease';
-		this._toast.style.boxShadow = '0 0 20px rgba(0, 0, 0, 0.6)';
-
-		this._toastContainer = document.createElement('div');
-		this._toastDiv.style.right = '20px';
-		this._toastDiv.style.bottom = '20px';
-		this._toastDiv.style.position = 'fixed';
-		document.body.appendChild(this._toastDiv);
-
-		// Set animation
-		let time = 1000;
-		let type = 'opacity';
-		let speedCurve = 'ease';
-
-		this.animationTime = time;
-
-		if (animation) {
-			if (animation.fadeTime == null || animation.fadeTime <= 0) {
-				console.warn('Animation time is not set, resetting to default');
-			} else if (!animation.speedCurve) {
-				console.warn('Animation speed curve is not set, resetting to default');
-			} else if (!animation.type) {
-				console.warn('Animation type is not set, resetting to default');
-			}
-
-			time = animation.fadeTime / 1000;
-			speedCurve = animation.speedCurve;
-		}
-
-		switch (type) {
-			case 'botom':
-				this._toast.style.bottom = '-110%';
-				this._toast.style.transition = `bottom ${time}s ${speedCurve}`;
-				break;
-			case 'opacity':
-				this._toast.style.opacity = '0';
-				this._toast.style.transition = `opacity ${time}s ${speedCurve}`;
-				break;
-			default:
-				this._toast.style.opacity = '0';
-				this._toast.style.transition = `opacity ${time}s ${speedCurve}`;
-				break;
-		}
-
-		// Set buttons
-		if (buttons) {
-			let buttonDiv = document.createElement('div');
-
-			buttons.forEach( (object, key) => {
-				if (object.name == null || object.name =='') {
-					console.error('Button name is not set');
-				} else {
-					let button = document.createElement('button');
-					button.innerText = object.name;
-
-					// object.clickFunction
-
-					buttonDiv.appendChild(button);
-
-					button.addEventListener('click', function() {
-						console.log(button);
-						console.log('Lol');
-					}, false);
-				}
-			});
-
-			this._toast.appendChild(buttonDiv);
-		}
-
-		// Styles
-		if (styles) {
-			this._toastDiv.style.color = styles.textColor;
-			this._toastDiv.style.backgroundColor = styles.color;
-
-			if (!styles.textColor) {
-				this._toastDiv.style.color = 'white';
-				console.warn('Toast color is not set, resetting to default');
-			} else if (!styles.position) {
-				console.warn('Toast background color is not set, resetting to default');
-			} else if (!styles.color) {
-				this._toastDiv.style.backgroundColor = '#3B3B4F';
-				console.warn('Toast background color is not set, resetting to default');
-			}
-
-			// Set position
-			switch (styles.position) {
-				case 'topLeft':
-					this._toastDiv.style.top = '20px';
-					this._toastDiv.style.left = '20px';
-					break;
-				case 'topRight':
-					this._toastDiv.style.top = '20px';
-					this._toastDiv.style.right = '20px';
-					break;
-				case 'bottomLeft':
-					this._toastDiv.style.left = '20px';
-					this._toastDiv.style.bottom = '20px';
-					break;
-				case 'bottomRight':
-					this._toastDiv.style.right = '20px';
-					this._toastDiv.style.bottom = '20px';
-					break;
-				default:
-					this._toastDiv.style.right = '20px';
-					this._toastDiv.style.bottom = '20px';
-					break;
-			}
-		}
-	}
-
-	toast(text) {
-		if (text) {
-			this._toast.innerHTML += `<p>${text}</p>`;
-
-			this._toastDiv.appendChild(this._toast);
-
-			window.requestAnimationFrame( () => {
-				this._toast.style.opacity = '1';
-			});
-
-			setTimeout( () => {
-				this.remove();
-			}, this.stayTime);
-		} else {
-			console.warn('Text is not defined');
-			// console.error(Error('Text is not defined'));
-		}
-	}
-
-	remove() {
-		let check;
-		let animationTime = 1000;
-
-		if (this.animation) {
-			animationTime = this.animationTime;
-		}
-
-		this._toast.style.opacity = '0';
-
-		setTimeout( () => {
-			this._toast.style.display = 'none';
-			this._toastDiv.removeChild(this._toast);
-		}, animationTime + 100);
-
-		try {
-			check = document.body.querySelector(this._toast);
-		} catch (err) {
-			return true;
-		}
-
-		if (check) {
-			console.error('Something happended, errorcode 1');
-			return false;
-		}
-	}
-}*/
