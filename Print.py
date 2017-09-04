b0a2511836cb46f6cffacafa63538e86d3074183
Vic <mail@testsite-vic.ml>
2017-09-04 13:37:52 +0200

    Update Print.py

--- Print.py
+++ Print.py
@@ -7,7 +7,8 @@ while(True):
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((TCP_IP, TCP_PORT))
-		s.sendall(b' ')
+		s.sendall(content.encode())
+		s.shutdown(socket.SHUT_WR)
 		s.close()
 
 		print(s)
