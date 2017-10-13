201d7b07175b209a793666c17d0f50cef7be0cf5
Unknown <rutjensstijn@gmail.com>
2017-10-13 19:01:32 +0200

    remove dead code

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -1,3 +1,27 @@
+/*
+    MIT License
+
+    Copyright (c) 2017 Vic
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+ */
+
 let int;
 let canvas;
 let mouseX = 0;
@@ -21,10 +45,6 @@ function load() {
 function loop() {
 	if ((typeof draw).toLowerCase() == 'function') {
 		looping = true;
-// 		int = setInterval(() => {
-// 			draw();
-// 			frameCount++;
-// 		}, 1000 / frameRate);
 		
 		function l() {
 			if (looping) {
@@ -40,7 +60,6 @@ function loop() {
 
 function noLoop() {
 	looping = false;
-// 	clearInterval(int);
 }
 
 function preventLoop() {
